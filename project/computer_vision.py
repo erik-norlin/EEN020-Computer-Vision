bc5cdba448f56433a3b1f3cd14e1c332a9edad64
eriknorlin <109854137+erik-norlin@users.noreply.github.com>
2023-12-23 16:30:52 +0100

    T_DLT done

--- project/computer_vision.py
+++ project/computer_vision.py
@@ -58,11 +58,6 @@ def compute_normalized_principal_axis(P):
     v = LA.det(M) * m3
     return normalize_vector(v)
 
-# def compute_camera_center_and_normalized_principal_axis(P):
-#     C = compute_camera_center(P)
-#     p_axis = compute_normalized_principal_axis(P) 
-#     return C, p_axis
-
 def compute_camera_center_and_normalized_principal_axis(P, multi=False):
 
     if multi:
@@ -127,30 +122,27 @@ def find_mean_and_std(x, axis):
     std =  (np.sum((x[axis,:] - mean)**2) / np.size(x,1))**0.5
     return mean, std
 
-def normalize_x_and_y(img_pts):
+def normalize_x_and_y(img_pts, verbose=False):
 
-    axis = 0
-    x_mean, x_std = find_mean_and_std(img_pts, axis)
-
-    axis = 1
-    y_mean, y_std = find_mean_and_std(img_pts, axis)
-
-    print('\nx_mean:', x_mean, '\nx_std:', x_std, '\ny_mean:', y_mean, '\ny_std:', y_std)
+    x_mean, x_std = find_mean_and_std(img_pts, axis=0)
+    y_mean, y_std = find_mean_and_std(img_pts, axis=1)
 
     N = np.array([[1/x_std, 0, -x_mean/x_std],
                     [0, 1/y_std, -y_mean/y_std],
                     [0, 0, 1]])
     
     img_pts_norm = N @ img_pts
-    
-    print('\nx_mean:', np.mean(img_pts_norm[0,:]), '\nx_std:', np.std(img_pts_norm[0,:]), '\ny_mean:', np.mean(img_pts_norm[1,:]), '\ny_std:', np.std(img_pts_norm[1,:]))
+
+    if verbose:
+        print('\nx_mean:', x_mean, 'x_std:', x_std, 'y_mean:', y_mean, 'y_std:', y_std)
+        print('x_mean:', np.mean(img_pts_norm[0,:]), 'x_std:', np.std(img_pts_norm[0,:]), 'y_mean:', np.mean(img_pts_norm[1,:]), 'y_std:', np.std(img_pts_norm[1,:]))
 
     return img_pts_norm, N
 
 def check_mean_and_std(x):
     print('\nx_mean:', np.mean(x[0,:]), '\nx_std:', np.std(x[0,:]), '\ny_mean:', np.mean(x[1,:]), '\ny_std:', np.std(x[1,:]))
 
-def estimate_camera_DLT(Xmodel, img_pts, print_svd=False):
+def estimate_camera_DLT(Xmodel, img_pts, verbose=False):
 
     n = np.size(img_pts,1)
     M = []
@@ -173,7 +165,7 @@ def estimate_camera_DLT(Xmodel, img_pts, print_svd=False):
     U, S, VT = LA.svd(M, full_matrices=False)
     P = np.stack([VT[-1, i:i+4] for i in range(0, 12, 4)], 0)
 
-    if print_svd:
+    if verbose:
         M_approx = U @ np.diag(S) @ VT
         v = VT[-1,:] # last row of VT because optimal v should be last column of V
         Mv = M @ v
@@ -184,7 +176,7 @@ def estimate_camera_DLT(Xmodel, img_pts, print_svd=False):
 
     return P
 
-def triangulate_3D_point_DLT(P1, P2, img1_pts, img2_pts, print_svd=False):
+def triangulate_3D_point_DLT(P1, P2, img1_pts, img2_pts, verbose=False):
     
     n = np.size(img1_pts,1)
     X = []
@@ -205,9 +197,9 @@ def triangulate_3D_point_DLT(P1, P2, img1_pts, img2_pts, print_svd=False):
         U, S, VT = LA.svd(M, full_matrices=False)
         X.append(VT[-1,:])
         
-        if print_svd:
+        if verbose:
             M_approx = U @ np.diag(S) @ VT
-            v = VT[-1,:] # last row of VT because optimal v should be last column of V
+            v = VT[-1,:]
             Mv = M @ v
             print('\n||Mv||:', (Mv @ Mv)**0.5)
             print('||v||^2:', v @ v)
@@ -217,8 +209,8 @@ def triangulate_3D_point_DLT(P1, P2, img1_pts, img2_pts, print_svd=False):
     X = dehomogenize(np.stack(X,1))
     return X # in P3
 
-def compute_triangulated_X_from_extracted_P2_solutions(P1, P2_arr, x1_norm, x2_norm):
-    X_arr = np.array([triangulate_3D_point_DLT(P1, P2, x1_norm, x2_norm, print_svd=False) for P2 in P2_arr])
+def compute_triangulated_X_from_extracted_P2_solutions(P1, P2_arr, x1_norm, x2_norm, verbose=False):
+    X_arr = np.array([triangulate_3D_point_DLT(P1, P2, x1_norm, x2_norm, verbose=verbose) for P2 in P2_arr])
     return X_arr
 
 def enforce_fundamental(F):
@@ -229,9 +221,9 @@ def enforce_fundamental(F):
     F = U @ np.diag(S) @ VT
     return F  
 
-def estimate_F_DLT(img_pts_1, img_pts_2, enforce=False, verbose=False): # Computes F such that x2.T @ F @ x1 = 0
+def estimate_F_DLT(img1_pts, img2_pts, enforce=False, verbose=False): # Computes F such that x2.T @ F @ x1 = 0
 
-    n = np.size(img_pts_1,1)
+    n = np.size(img1_pts,1)
     M = []
 
     for i in range(n):
@@ -246,8 +238,8 @@ def estimate_F_DLT(img_pts_1, img_pts_2, enforce=False, verbose=False): # Comput
 
         # m = np.array([[x2*x1, x2*y1, x2*z1, y2*x1, y2*y1, y2*z1, z2*x1, z2*y1, z2*z1]])
 
-        x = img_pts_1[:,i]
-        y = img_pts_2[:,i]
+        x = img1_pts[:,i]
+        y = img2_pts[:,i]
         m = np.outer(y, x).flatten()
         M.append([m])
 
@@ -259,13 +251,13 @@ def estimate_F_DLT(img_pts_1, img_pts_2, enforce=False, verbose=False): # Comput
         F = enforce_fundamental(F)
 
     if verbose:
-        for i in range(np.size(img_pts_1,1)):
-            epi_const = img_pts_2[:,i].T @ F @ img_pts_1[:,i]
+        for i in range(np.size(img1_pts,1)):
+            epi_const = img2_pts[:,i].T @ F @ img1_pts[:,i]
             print('x2^T @ F @ x1:', epi_const)
         
         print('\nDet(F):', LA.det(F))
         M_approx = U @ np.diag(S) @ VT
-        v = VT[-1,:] # last row of VT because optimal v should be last column of V
+        v = VT[-1,:]
         Mv = M @ v
         print('||Mv||:', (Mv @ Mv)**0.5)
         print('||v||^2:', v @ v)
@@ -282,15 +274,15 @@ def enforce_essential(E):
     E =  U @ np.diag([1,1,0]) @ VT
     return E
 
-def estimate_E_DLT(img_pts_1, img_pts_2, enforce=False, verbose=False):
+def estimate_E_DLT(img1_pts_norm, img2_pts_norm, enforce=False, verbose=False):
 
-    n = np.size(img_pts_1,1)
+    n = np.size(img1_pts_norm,1)
     M = []
 
     for i in range(n):
 
-        x = img_pts_1[:,i]
-        y = img_pts_2[:,i]
+        x = img1_pts_norm[:,i]
+        y = img2_pts_norm[:,i]
         m = np.outer(y, x).flatten()
         M.append([m])
 
@@ -301,13 +293,13 @@ def estimate_E_DLT(img_pts_1, img_pts_2, enforce=False, verbose=False):
         E = enforce_essential(E)
 
     if verbose:
-        for i in range(np.size(img_pts_1,1)):
-            epi_const = img_pts_2[:,i].T @ E @ img_pts_1[:,i]
+        for i in range(np.size(img1_pts_norm,1)):
+            epi_const = img2_pts_norm[:,i].T @ E @ img1_pts_norm[:,i]
             print('x2^T @ E @ x1:', epi_const)
         
         print('\nDet(E):', LA.det(E))
         M_approx = U @ np.diag(S) @ VT
-        v = VT[-1,:] # last row of VT because optimal v should be last column of V
+        v = VT[-1,:]
         Mv = M @ v
         print('||Mv||:', (Mv @ Mv)**0.5)
         print('||v||^2:', v @ v)
@@ -329,16 +321,14 @@ def estimate_E_robust(K, x1_norm, x2_norm, n_its, n_samples, err_threshold_px):
         rand_mask = np.random.choice(np.size(x1_norm,1), n_samples, replace=False)
         E = estimate_E_DLT(x1_norm[:,rand_mask], x2_norm[:,rand_mask], enforce=True, verbose=False)
 
-        D1, D2 = compute_epipolar_errors(E, x1_norm, x2_norm)
-        inliers = ((D1**2 + D2**2) / 2) < err_threshold**2
-
+        distance1_arr, distance2_arr = compute_epipolar_errors(E, x1_norm, x2_norm)
+        inliers = ((distance1_arr**2 + distance2_arr**2) / 2) < err_threshold**2
         n_inliers = np.sum(inliers)
 
         if n_inliers > max_inliers:
             best_inliers = np.copy(inliers)
             best_E = np.copy(E)
             max_inliers = n_inliers
-
             print(np.sum(inliers), end='\r')
         
     return best_E, best_inliers
@@ -352,9 +342,10 @@ def compute_ransac_iterations(alpha, epsilon, s, min_its, max_its, scale):
     return T
 
 def normalize_camera(P):
-    P= P/P[-1,-1]
-    if P[2,2] < 0:
-        P = -P
+    if not np.isclose(P[-1,-1], 0):
+        P= P/P[-1,-1]
+        if P[2,2] < 0:
+            P = -P
     return P
 
 def convert_E_to_F(E, K1, K2):
@@ -375,41 +366,50 @@ def compute_and_plot_lines(l, img, ax):
         x = np.linspace(0, np.size(img,1), 2)
         y = (-a*x - c) / b # ax + by + c = 0 ==> y = (-ax - c) / b
 
-        ax.plot(x, y, '-', lw=3, color=col[i], alpha=0.7) #  label='Line {}'.format(i+1)
+        ax.plot(x, y, '-', lw=3, color=col[i], alpha=0.7)
 
-def compute_RMS_error(D1, D2):
-    e_rms = np.sqrt(np.sum(D1**2 + D2**2) / (2*np.size(D1, 0)))
+def compute_RMS_error(distance1_arr, distance2_arr):
+    e_rms = np.sqrt(np.sum(distance1_arr**2 + distance2_arr**2) / (2*np.size(D1, 0)))
     return e_rms
 
-def compute_point_line_distance_2D(l, p):
-    a = l[0,:]
-    b = l[1,:]
-    c = l[2,:]
+def compute_point_line_distance_2D(l, p, multi=False):
 
-    x = p[0,:]
-    y = p[1,:]
+    if multi:
+        a = l[0,:]
+        b = l[1,:]
+        c = l[2,:]
 
-    D = np.abs(a*x + b*y + c) / (a**2 + b**2)**0.5
+        x = p[0,:]
+        y = p[1,:]
+    else:
+        a = l[0]
+        b = l[1]
+        c = l[2]
+
+        x = p[0]
+        y = p[1]
+    
+    distance_arr = np.abs(a*x + b*y + c) / (a**2 + b**2)**0.5
 
     # numerator = np.abs(np.einsum("ij, ij->j", p, l))
     # denominator = LA.norm(l[:-1], axis=0)
     # D = numerator / denominator
 
-    return D
+    return distance_arr
 
 def compute_epipolar_lines(F, x1, x2):
-    # l3 = F @ x1
-    # l4 = F.T @ x2
+    l2 = F @ x1
+    l1 = F.T @ x2
 
-    l2 = np.einsum("ij,jk->ik", F, x1)
-    l1 = np.einsum("ji,jk->ik", F, x2)
+    # l2 = np.einsum("ij,jk->ik", F, x1)
+    # l1 = np.einsum("ji,jk->ik", F, x2)
     return l1, l2
 
 def compute_epipolar_errors(F, x1, x2):
     l1, l2 = compute_epipolar_lines(F, x1, x2)
-    D1 = compute_point_line_distance_2D(l1, x1)
-    D2 = compute_point_line_distance_2D(l2, x2)
-    return D1, D2
+    distance1_arr = compute_point_line_distance_2D(l1, x1, multi=True)
+    distance2_arr = compute_point_line_distance_2D(l2, x2, multi=True)
+    return distance1_arr, distance2_arr
 
 def RQ_decompose(a):
 
@@ -430,7 +430,7 @@ def RQ_decompose(a):
 
     return r, q
 
-def compute_sift_points(img1, img2, marg):
+def compute_sift_points(img1, img2, marg, verbose=False):
     img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
     # img1 = cv2.medianBlur(img1, ksize = 5)
     # img1 = cv2.normalize(img1, None, 0, 255, cv2.NORM_MINMAX)
@@ -457,8 +457,6 @@ def compute_sift_points(img1, img2, marg):
     # bf = cv2.BFMatcher()
     # matches = bf.knnMatch(des1, des2, k=2)
 
-    print('Number of matches:', np.size(matches,0))
-
     good_matches = []
     for m, n in matches:
         if m.distance < marg*n.distance:
@@ -473,25 +471,19 @@ def compute_sift_points(img1, img2, marg):
     x1 = homogenize(x1, multi=True)
     x2 = homogenize(x2, multi=True)
 
-    print('Number of good matches:', np.size(x1,1))
+    x1 = np.stack([des1[match[0].queryIdx] for match in good_matches],1)
+    
+
+    if verbose:
+        print('Number of matches:', np.size(matches,0))
+        print('Number of good matches:', np.size(x1,1))
 
     return x1, x2, kp1, kp2, des1, des2, img_match
 
-def compute_sift_points_sequential(kp1, des1, img2, marg):
-    # img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-    # img1 = cv2.medianBlur(img1, ksize = 5)
-    # img1 = cv2.normalize(img1, None, 0, 255, cv2.NORM_MINMAX)
-    # img1 = img1.astype(np.float32)
-
+def compute_sift_points_sequential(kp1, des1, img2, marg, verbose=False):
     img2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
-    # img2 = cv2.medianBlur(img2, ksize = 5)
-    # img2 = cv2.normalize(img2, None, 0, 255, cv2.NORM_MINMAX)
-    # img2 = img2.astype(np.float32)
 
-
-    # sift = cv2.SIFT_create(int nfeatures=0, int nOctaveLayers=3, double contrastThreshold=0.04, double edgeThreshold=10, double sigma=1.6)
     sift = cv2.SIFT_create()
-    # kp1, des1 = sift.detectAndCompute(img1, None)
     kp2, des2 = sift.detectAndCompute(img2, None)
 
     FLANN_INDEX_KDTREE = 1
@@ -501,18 +493,10 @@ def compute_sift_points_sequential(kp1, des1, img2, marg):
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1, des2, k=2)
 
-    # bf = cv2.BFMatcher()
-    # matches = bf.knnMatch(des1, des2, k=2)
-
-    print('Number of matches:', np.size(matches,0))
-
     good_matches = []
     for m, n in matches:
         if m.distance < marg*n.distance:
             good_matches.append([m])
-
-    # draw_params = dict(matchColor=(255,0,255), singlePointColor=(0,255,0), matchesMask=None, flags=cv2.DrawMatchesFlags_DEFAULT)
-    # img_match = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good_matches, None, **draw_params)
 
     x1 = np.stack([kp1[match[0].queryIdx].pt for match in good_matches],1)
     x2 = np.stack([kp2[match[0].trainIdx].pt for match in good_matches],1)
@@ -520,7 +504,9 @@ def compute_sift_points_sequential(kp1, des1, img2, marg):
     x1 = homogenize(x1, multi=True)
     x2 = homogenize(x2, multi=True)
 
-    print('Number of good matches:', np.size(x1,1))
+    if verbose:
+        print('Number of matches:', np.size(matches,0))
+        print('Number of good matches:', np.size(x1,1))
 
     return x1, x2
 
@@ -539,39 +525,27 @@ def plot_sift_points(x1, x2, img1, img_match, path, save=False):
     ax.set_xlabel('$x$')
     ax.set_ylabel('$y$')
     ax.set_aspect('equal')
-    # ax.legend(loc="upper right")
     ax.imshow(cv2.cvtColor(img_match, cv2.COLOR_BGR2RGB))
     fig.tight_layout()
     if save:
         fig.savefig(path, dpi=300)
     plt.show()
 
-def point_line_distance_2D(l, p):
-    a = l[0]
-    b = l[1]
-    c = l[2]
-
-    x = p[0]
-    y = p[1]
-
-    d = np.abs(a*x + b*y + c) / (a**2 + b**2)**0.5
-    return d
-
-def get_skew_symmetric_vector(T):
+def extract_skew_symmetric_vector(T):
     t = np.array([T[2,1], T[0,2], T[1,0]])
     return t
 
-def get_skew_symmetric_matrix(t):
+def create_skew_symmetric_matrix(t):
     T = np.array([[0, -t[2], t[1]],
                   [t[2], 0, -t[0]],
                   [-t[1], t[0], 0]])
     return T
 
 def compute_E_from_R_and_T(R, T):
-    E = get_skew_symmetric_matrix(T) @ R
+    E = create_skew_symmetric_matrix(T) @ R
     return E
 
-def extract_valid_camera_and_points(P1, P_arr, X_arr):
+def extract_valid_camera_and_points(P1, P_arr, X_arr, verbose=False):
         
     x1_arr = np.array([transform(P1, X) for X in X_arr])
     valid_coords_P1 = np.array([np.sum(x[-1] > 0) for x in x1_arr])
@@ -585,8 +559,9 @@ def extract_valid_camera_and_points(P1, P_arr, X_arr):
     P2_valid = P_arr[valid_coords_ind]
     P2_valid = P2_valid / P2_valid[-1,-1]
     
-    print('No. valid coords for each camera pair:', valid_coords)
-    print('Argmax(P2_arr):', valid_coords_ind)
+    if verbose:
+        print('No. valid coords for each camera pair:', valid_coords)
+        print('Argmax(P2_arr):', valid_coords_ind)
     
     return P2_valid, X_valid
 
@@ -618,8 +593,8 @@ def extract_P_from_E(E):
     R1 = U @ W @ VT
     R2 = U @ W.T @ VT
 
-    t1 = get_skew_symmetric_vector(S1)
-    t2 = get_skew_symmetric_vector(S2)
+    t1 = extract_skew_symmetric_vector(S1)
+    t2 = extract_skew_symmetric_vector(S2)
 
     P1 = np.concatenate((R1, t1[:, np.newaxis]), 1)
     P2 = np.concatenate((R1, t2[:, np.newaxis]), 1)
@@ -646,7 +621,7 @@ def compute_reprojection_error(P1, P2, Xj, x1j, x2j):
 
     return reproj_err, res
 
-def compute_total_reprojection_error(P1, P2, X_est, x1, x2):
+def compute_total_reprojection_error(P1, P2, X_est, x1, x2, verbose=False):
 
     n_pts = np.size(X_est,1)
     reproj_err_tot = []
@@ -664,9 +639,10 @@ def compute_total_reprojection_error(P1, P2, X_est, x1, x2):
     
     res_tot = np.concatenate(res_tot, 0)
 
-    print('\nTotal reprojection error:', round(np.sum(reproj_err_tot), 2))
-    print('Median reprojection error:', round(np.median(reproj_err_tot), 2))
-    print('Avg. reprojection error:', round(np.mean(reproj_err_tot), 2))
+    if verbose:
+        print('\nTotal reprojection error:', round(np.sum(reproj_err_tot), 2))
+        print('Median reprojection error:', round(np.median(reproj_err_tot), 2))
+        print('Avg. reprojection error:', round(np.mean(reproj_err_tot), 2))
 
     return reproj_err_tot, res_tot
 
@@ -756,7 +732,6 @@ def plot_cameras_and_axes(ax, C_list, axis_list, s):
         y_axis = C[1] + s*axis[1]
         z_axis = C[2] + s*axis[2]
 
-        # ax.plot(x_axis, y_axis, z_axis, 'o', label='Axis')
         ax.plot([x_axis, C[0]], [y_axis, C[1]], [z_axis, C[2]], '-', color=col[i], lw=3, alpha=0.7)
 
 def plot_cameras_and_3D_points(X, C_arr, axis_arr, s, path):
